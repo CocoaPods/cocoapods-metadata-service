94e97a483fcc5adaacc125813c9487911ccc5155
Orta Therox <orta.therox@gmail.com>
2018-02-26 19:49:55 -0500

    Handles the trunk secret webhook URL

--- source/server.ts
+++ source/server.ts
@@ -1,5 +1,6 @@
 import * as bodyParser from "body-parser"
 import * as express from "express"
+import { TRUNK_WEBHOOK_SECRET } from "./globals"
 import { trunkWebhook } from "./webhook"
 
 export const createApp = () => {
@@ -7,6 +8,6 @@ export const createApp = () => {
   app.set("port", process.env.PORT || 5000)
   app.use(bodyParser.json())
 
-  app.post("/webhook", trunkWebhook)
+  app.post(`/webhook/${TRUNK_WEBHOOK_SECRET}`, trunkWebhook)
   return app
 }
